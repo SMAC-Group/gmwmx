89128e89cb22271d9243535fec83312acbe0ee62
lionelvoirol <lionelvoirol@hotmail.com>
2022-07-07 16:44:53 +0200

    remove dependency to infuser and to the development version of simts

--- R/hector.R
+++ R/hector.R
@@ -1,3 +1,69 @@
+# function to infuse remove outliers template
+#' @importFrom stringi stri_replace
+infuse_remove_outliers_tmp = function(data_file,
+                                      working_folder,
+                                      output_file,
+                                      n_seasonal,
+                                      IQ_factor,
+                                      x){
+  
+  
+  remove_outliers_tmpl = "DataFile              data_file_value\nDataDirectory         data_directory_value\nOutputFile            output_file_value\ninterpolate           no\nseasonalsignal        seasonal_signal_value\nhalfseasonalsignal    half_seasonal_signal_value\nestimateoffsets       estimate_offsets_value\nIQ_factor             iq_factor_value\nPhysicalUnit          mm\n"
+  
+  # replace data_file_value by data_file
+  tpl_1 = stringi::stri_replace( str = remove_outliers_tmpl, regex =  "data_file_value" , replacement = data_file)
+  
+  tpl_2 = stringi::stri_replace( str = tpl_1,regex =  "data_directory_value" , replacement = working_folder)
+  
+  tpl_3 = stringi::stri_replace( str = tpl_2,regex =  "output_file_value" , replacement = output_file)
+  
+  tpl_4 = stringi::stri_replace( str = tpl_3,regex =  "seasonal_signal_value" , replacement = if (n_seasonal > 0) "yes" else "no")
+  
+  tpl_5 = stringi::stri_replace( str = tpl_4,regex =  "half_seasonal_signal_value" , replacement = if (n_seasonal > 1) "yes" else "no")
+  
+  tpl_6 = stringi::stri_replace( str = tpl_5,regex =  "estimate_offsets_value" , replacement = if (!is.null(x$jumps[1])) "yes" else "no")
+  
+  tpl_7 = stringi::stri_replace( str = tpl_6,regex =  "iq_factor_value" , replacement = sprintf('%.2f', IQ_factor))
+  
+  tpl_7
+}
+
+# function to infuse estimate trend template
+#' @importFrom stringi stri_replace
+infuse_estimate_trend_tmp = function(data_file,
+                                     working_folder,
+                                     output_file,
+                                     noise_model,
+                                     n_seasonal,
+                                     x){
+  
+  estimate_trend_tmpl = "DataFile              data_file_value\nDataDirectory         data_directory_value\nOutputFile            output_file_value\ninterpolate           no\nPhysicalUnit          mm\nScaleFactor           1.0\nNoiseModels           noise_models_value\nseasonalsignal        seasonal_signal_value\nhalfseasonalsignal    half_seasonal_signal_value\nestimateoffsets       estimate_offsets_value\nestimatepostseismic   no\nestimateslowslipevent no\nRandomiseFirstGuess   no\nTimeNoiseStart        1\nDegreePolynomial      degree_polynomial_value\nLikelihoodMethod      FullCov\nJSON                  yes\n"
+  
+  # replace data_file_value by data_file
+  tpl_1 = stringi::stri_replace( str = estimate_trend_tmpl, regex =  "data_file_value" , replacement = data_file)
+  
+  tpl_2 = stringi::stri_replace( str = tpl_1,regex =  "data_directory_value" , replacement = working_folder)
+  
+  tpl_3 = stringi::stri_replace( str = tpl_2,regex =  "output_file_value" , replacement = output_file)
+  
+  tpl_4 = stringi::stri_replace( str = tpl_3,regex =  "noise_models_value" , replacement = noise_model)
+  
+  tpl_5 = stringi::stri_replace( str = tpl_4,regex =  "seasonal_signal_value" , replacement = if (n_seasonal > 0) "yes" else "no")
+  
+  tpl_6 = stringi::stri_replace( str = tpl_5,regex =  "half_seasonal_signal_value" , replacement = if (n_seasonal > 1) "yes" else "no")
+  
+  tpl_7 = stringi::stri_replace( str = tpl_6,regex =  "estimate_offsets_value" , replacement = if (!is.null(x$jumps[1])) "yes" else "no")
+  
+  tpl_8 = stringi::stri_replace( str = tpl_7,regex =  "degree_polynomial_value" , replacement = 1)
+  
+  tpl_8
+}
+
+
+
+
+
+
 #' Estimate a stochastic model based on the MLE and the Hector implementation.
 #' 
 #' @param x A \code{gnssts} object
@@ -8,7 +74,6 @@
 #' @export
 #' @importFrom rjson fromJSON
 #' @importFrom stringi stri_rand_strings
-#' @importFrom infuser infuse
 #' @importFrom wv wvar
 estimate_hector <- function(
     x,
@@ -45,20 +110,27 @@ estimate_hector <- function(
   cfg_file = paste(working_folder, "estimate.ctl", sep="/")
   output_file = paste(working_folder, "output.mom", sep="/")
   
+  cfg = infuse_estimate_trend_tmp(data_file = "ts.mom",
+                                  working_folder = working_folder,
+                                  output_file = output_file,
+                                  noise_model = gen_hector_model(model), 
+                                  n_seasonal = n_seasonal, 
+                                  x = x)
+  
   # infuse hector configuration file using file saved in R/sysdata.rda
-  cfg = infuse(
-    file_or_string  = estmatetrend_tmpl,
-    list(
-      DataFile = "ts.mom", 
-      DataDirectory = working_folder, 
-      OutputFile = output_file,
-      NoiseModels = gen_hector_model(model),
-      seasonalsignal = if (n_seasonal > 0) "yes" else "no",
-      halfseasonalsignal = if (n_seasonal > 1) "yes" else "no",
-      estimateoffsets = if (!is.null(x$jumps[1])) "yes" else "no",
-      DegreePolynomial = 1
-    )
-  )
+  # cfg = infuse(
+  #   file_or_string  = estmatetrend_tmpl,
+  #   list(
+  #     DataFile = "ts.mom", 
+  #     DataDirectory = working_folder, 
+  #     OutputFile = output_file,
+  #     NoiseModels = gen_hector_model(model),
+  #     seasonalsignal = if (n_seasonal > 0) "yes" else "no",
+  #     halfseasonalsignal = if (n_seasonal > 1) "yes" else "no",
+  #     estimateoffsets = if (!is.null(x$jumps[1])) "yes" else "no",
+  #     DegreePolynomial = 1
+  #   )
+  # )
   
   # write cfg file
   write(cfg, file = cfg_file)
@@ -166,7 +238,6 @@ estimate_hector <- function(
 #' @return A \code{gnssts} object.
 #' @export
 #' @importFrom stringi stri_rand_strings
-#' @importFrom infuser infuse
 remove_outliers_hector <- function(x, n_seasonal, IQ_factor = 3, cleanup = T) {
   
   if (!("gnssts" %in% class(x))) {
@@ -192,19 +263,26 @@ remove_outliers_hector <- function(x, n_seasonal, IQ_factor = 3, cleanup = T) {
 
   cfg_file = paste(working_folder, "removeoutliers.ctl", sep="/")
   
+  cfg = infuse_remove_outliers_tmp(data_file = "ts.mom",
+                                   working_folder = working_folder,
+                                   output_file = "ts_out.mom", 
+                                   n_seasonal = n_seasonal, 
+                                   IQ_factor = IQ_factor,
+                                   x = x)
+  
 # infuse using the template removeoutliers_tmpl accesible in R/sysdata.rda
-  cfg = infuse(
-    file_or_string = removeoutliers_tmpl,
-    list(
-      data_file = "ts.mom",
-      data_dir = working_folder,
-      output_file = "ts_out.mom",
-      seasonalsignal = if (n_seasonal > 0) "yes" else "no",
-      halfseasonalsignal = if (n_seasonal > 1) "yes" else "no",
-      estimateoffsets = if (!is.null(x$jumps[1])) "yes" else "no",
-      IQ_factor = sprintf('%.2f', IQ_factor)
-    )
-  )
+  # cfg = infuse(
+  #   file_or_string = removeoutliers_tmpl,
+  #   list(
+  #     data_file = "ts.mom",
+  #     data_dir = working_folder,
+  #     output_file = "ts_out.mom",
+  #     seasonalsignal = if (n_seasonal > 0) "yes" else "no",
+  #     halfseasonalsignal = if (n_seasonal > 1) "yes" else "no",
+  #     estimateoffsets = if (!is.null(x$jumps[1])) "yes" else "no",
+  #     IQ_factor = sprintf('%.2f', IQ_factor)
+  #   )
+  # )
   
   write(cfg, file = cfg_file)
   
