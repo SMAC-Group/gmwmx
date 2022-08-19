85f4dc2784b29d38a18fcb27d66301efb0140d73
lionelvoirol <lionelvoirol@hotmail.com>
2022-08-19 15:53:04 +0200

    modified vignette load_estimate_compare_models so that there are no estimation ran when building it. Transfert required output in inst/extdata

--- data-raw/save_hector_template_as_internal_data.R
+++ data-raw/save_hector_template_as_internal_data.R
@@ -1,5 +1,5 @@
 ## save template for running hector functions
-
+# this could be remove as now the template is directly defined in functions infuse_remove_outliers_tmp() and infuse_estimate_trend_tmp() in R/hector.R
 estmatetrend_tmpl = readr::read_file("data-raw/estimatetrend.tmpl")
 removeoutliers_tmpl = readr::read_file("data-raw/removeoutliers.tmpl")
 usethis::use_data(estmatetrend_tmpl, removeoutliers_tmpl, internal = TRUE, overwrite = T)
